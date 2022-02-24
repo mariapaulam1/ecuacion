1c48130c9cf232d19253c4552df45cfe734f3ddf
mariapaulam1 <98542876+mariapaulam1@users.noreply.github.com>
2022-02-24 11:58:18 -0500

    Add files via upload

---
+++ ecuaciones2.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<meta charset="utf-8">
+<title>ecuación</title>
+</head>
+
+<body> 
+<H2>Resolucion de una ecuación de segundo grado. </H2>
+	
+<img src="formula.jpg">	
+
+<form method="POST" action="procesa.php"> 
+    <p>a = <input type="text" name="a" size="5"></p> 
+    <p>b = <input type="text" name="b" size="5"></p> 
+    <p>c = <input type="text" name="c" size="5"></p> 
+    <p><input type="reset" value="Restablecer" name="B2">&nbsp; <input type="submit" value="Calcular" name="B1"></p> 
+</form> 
+
+</body> 
+
+</html> 
+
+
+
